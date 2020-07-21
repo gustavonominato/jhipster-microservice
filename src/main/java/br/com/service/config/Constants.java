fafdf307277428ddad65eadd5585ec22372aa70f
Luiz Gustavo Silveira Nominato <gustavonominato@gmail.com>
2020-07-21 18:00:58 -0300

    Initial version of service generated by JHipster-6.7.1

---
+++ src/main/java/br/com/service/config/Constants.java
@@ -0,0 +1,12 @@
+package br.com.service.config;
+
+/**
+ * Application constants.
+ */
+public final class Constants {
+
+    public static final String SYSTEM_ACCOUNT = "system";
+
+    private Constants() {
+    }
+}
---
+++ src/main/java/br/com/service/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package br.com.service.config.audit;
